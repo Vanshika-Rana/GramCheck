95a30d5e7209b8fe838eb0e3becf696365e326b2
Vanshika Rana <ranavanshika172000@gmail.com>
2021-10-25 22:29:21 +0530

    adding files

--- app.py
+++ app.py
@@ -2,10 +2,10 @@ from gingerit.gingerit import GingerIt
 import streamlit as st
 from PIL import Image
 st.set_page_config(page_title='GramCheck', page_icon="./icon.png")
-
+header = Image.open('illus.jpg')
+st.image(header)
 st.title("GramCheck : Easy Way To Get Your English Sentences Corrected.")
-header = Image.open('illus.png')
-st.image(header, width=500)
+
 sentence = st.text_area(label="Enter Your Sentence Here ...")
 parser = GingerIt()
 
